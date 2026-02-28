5d4cddb67c785a9919ed0ed25fdc0a5648f84c31
abboskarvonov <abbos_karvonov1995@icloud.com>
2026-02-28 19:00:48 +0500

    fix: include public/build in git for server deployment

---
+++ public/build/assets/ApplicationLogo-2dui4gdk.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-tiptap-CymQZA3-.js";function t(){return o.jsx("h1",{children:"BHMI"})}export{t as A};
---
+++ public/build/assets/RenderError-BEpkqMnU.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-tiptap-CymQZA3-.js";const n=(r,s)=>r[s]?t.jsx("span",{className:"text-red-600",children:r[s]}):null;export{n as r};
